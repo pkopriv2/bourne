f97241ad87b9b341ab9807dba51f4aaaeeba012d
Preston Koprivica <pkopriv2@Jessicas-MBP.home>
2016-11-02 21:16:11 -0500

    Convoy: wip

--- convoy/interfaces.go
+++ convoy/interfaces.go
@@ -4,6 +4,7 @@ import (
 	"io"
 
 	"github.com/pkopriv2/bourne/common"
+	"github.com/pkopriv2/bourne/net"
 	uuid "github.com/satori/go.uuid"
 )
 
@@ -14,20 +15,28 @@ import (
 //  * https://www.cs.cornell.edu/~asdas/research/dsn02-swim.pdf
 //  * Basic Design: http://bitsavers.informatik.uni-stuttgart.de/pdf/xerox/parc/techReports/CSL-89-1_Epidemic_Algorithms_for_Replicated_Database_Maintenance.pdf
 //
-// Convoy is a database dissemination library.  The intended use is primarily
-// within infrastructure management, where the desire is to understand
-// not only what what hosts are currently alive and healthy, but also
-// meta information about that host.  In that vein, hosts are able to
-// publish information about themselves. Moreover, clients of the cluster
-// can coordinate with a host to update the host's database.
+// Convoy is a very specialized replicated database and dissemination library.
+//
+// The intended use is primarily within infrastructure management, where the
+// desire is to understand not only what what hosts are currently alive and
+// healthy, but also meta information about that host.  In that vein, hosts are
+// able to publish information about themselves.  This can range from standard
+// properties (e.g. hostname, fqdn, ip address, etc..) but can really be anything.
 //
 // Convoy replicates each store to all members of the cluster using
 // an epidemic style dissemination protocol.  The goal is a searchable
-// data store which identifies hosts - or its properties.
+// data store which identifies hosts - or their properties.
 //
+type ChangeType int
+
+const (
+	Add = 1
+	Del = 2
+)
 
 // Publishes the db to the given port.  This is the "first" member of the
-// cluster.
+// cluster and will not discovery anyone else until it is contacted.
+//
 func Publish(ctx common.Context, db Database, port int) (Cluster, error) {
 	return nil, nil
 }
@@ -37,24 +46,9 @@ func PublishTo(ctx common.Context, db Database, addr string) (Cluster, error) {
 	return nil, nil
 }
 
-// A cluster represents the aggregated view of all members' data stores.
-type Cluster interface {
-	io.Closer
-
-	// Returns a handle to the store of the given id.  Consumers
-	// must close the store or risk leaking resources.
-	GetStore(store uuid.UUID) (Store, error)
-
-	// Returns the value of the given key from the given store.
-	GetValue(store uuid.UUID, key string) (string, error)
-
-	// MUST DO:
-	// Add search functiionality.
-}
-
 // A database is really just an indexed log of changes.
 type Database interface {
-	Store
+	Id() uuid.UUID
 
 	// Returns a channel containing an ordered list of changes
 	// whose versions are greater than or equal to the given
@@ -62,22 +56,8 @@ type Database interface {
 	Log(int) <-chan Change
 }
 
-// A change is the funamental dissemination unit
-type ChangeType int
-
-const (
-	Publish    ChangeType = 0
-	Leave                 = 1
-	Offline               = 2
-	ItemUpdate            = 3
-	ItemDelete            = 4
-)
-
-//
-type Transaction struct {
-
-	// Which store is being updated
-	Re uuid.UUID
+// The fundamental unit of change within the published database.
+type Change struct {
 
 	// At what version was the store.
 	Version int
@@ -90,14 +70,33 @@ type Transaction struct {
 	Val string
 }
 
+// A cluster represents the aggregated view of all members' data stores.
+type Cluster interface {
+	io.Closer
+
+	// Returns a handle to the store of the given id.  Consumers
+	GetMember(id uuid.UUID) (Member, error)
+
+	// MUST DO:
+	// Add search functiionality.
+}
+
+// A simple client abstraction.
+type Member interface {
+	io.Closer
+
+	// Connects to the target of the client on the given port.
+	Connect(int) (net.Connection, error)
+
+	// Returns a store client.
+	Store() (Store, error)
+}
+
 // A very simple key,value store abstraction. Updates to the store
 // are transactionally safe - regardless of whether the store is
 // local or remote.
 type Store interface {
 	io.Closer
-
-	// A value
-	Id() uuid.UUID
 
 	// Returns the item value and version
 	Get(key string) (string, error)
--- convoy/updates.go
+++ convoy/updates.go
@@ -1,125 +1,118 @@
 package convoy
 
-import (
-	"github.com/pkg/errors"
-	"github.com/pkopriv2/bourne/common"
-	"github.com/pkopriv2/bourne/enc"
-	uuid "github.com/satori/go.uuid"
-)
-
-func newJoin(member Member) update {
-	return &join{member}
-}
-
-func newLeave(memberId uuid.UUID, version int) update {
-	return &leave{memberId, version}
-}
-
-func parseUpdate(ctx common.Context, r enc.Reader) (update, error) {
-	var typ string
-	if err := r.Read("type", &typ); err != nil {
-		return nil, errors.Wrap(err, "Unable to parse update")
-	}
-
-	switch typ {
-	case "join":
-		return readJoin(ctx, r)
-	case "leave":
-		return readLeave(ctx, r)
-	}
-
-	return nil, errors.New("Unable to parse update.  Invalid type.")
-}
-
-type leave struct {
-	memberId uuid.UUID
-	version  int
-}
-
-func (d *leave) Re() uuid.UUID {
-	return d.memberId
-}
-
-func (d *leave) Version() int {
-	return d.version
-}
-
-func (l *leave) Write(w enc.Writer) {
-	w.Write("type", "leave")
-	w.Write("re", l.memberId.String())
-	w.Write("version", l.version)
-}
-
-func (d *leave) Apply(r Roster) bool {
-	return r.leave(d.memberId, d.version)
-}
-
-func readLeave(ctx common.Context, r enc.Reader) (*leave, error) {
-	var version int
-	if err := r.Read("version", &version); err != nil {
-		return nil, errors.Wrap(err, "Unable to read leave update")
-	}
-
-	id, err := readUUID(r, "re")
-	if err != nil {
-		return nil, errors.Wrap(err, "Unable to read leave update")
-	}
-
-	return &leave{id, version}, nil
-}
-
-type join struct {
-	member Member
-}
-
-func (p *join) Re() uuid.UUID {
-	return p.member.Id()
-}
-
-func (p *join) Version() int {
-	return p.member.Version()
-}
-
-func (p *join) Write(w enc.Writer) {
-	w.Write("type", "join")
-	w.Write("member", p.member)
-}
-
-func (p *join) Apply(r Roster) bool {
-	return r.join(p.member)
-}
-
-func readJoin(ctx common.Context, r enc.Reader) (*join, error) {
-	var message enc.Message
-	if err := r.Read("member", &message); err != nil {
-		return nil, errors.Wrap(err, "Unable to read join update")
-	}
-
-	member, err := readMember(ctx, message)
-	if err != nil {
-		return nil, errors.Wrap(err, "Unable to read join update")
-	}
-
-	return &join{member}, nil
-}
-
-
-// func newFail(member Member) update {
-// return &fail{member}
+// func newJoin(member Member) update {
+// return &join{member}
 // }
 //
-// type fail struct {
+// func newLeave(memberId uuid.UUID, version int) update {
+// return &leave{memberId, version}
+// }
+//
+// func parseUpdate(ctx common.Context, r enc.Reader) (update, error) {
+// var typ string
+// if err := r.Read("type", &typ); err != nil {
+// return nil, errors.Wrap(err, "Unable to parse update")
+// }
+//
+// switch typ {
+// case "join":
+// return readJoin(ctx, r)
+// case "leave":
+// return readLeave(ctx, r)
+// }
+//
+// return nil, errors.New("Unable to parse update.  Invalid type.")
+// }
+//
+// type leave struct {
+// memberId uuid.UUID
+// version  int
+// }
+//
+// func (d *leave) Re() uuid.UUID {
+// return d.memberId
+// }
+//
+// func (d *leave) Version() int {
+// return d.version
+// }
+//
+// func (l *leave) Write(w enc.Writer) {
+// w.Write("type", "leave")
+// w.Write("re", l.memberId.String())
+// w.Write("version", l.version)
+// }
+//
+// func (d *leave) Apply(r Roster) bool {
+// return r.leave(d.memberId, d.version)
+// }
+//
+// func readLeave(ctx common.Context, r enc.Reader) (*leave, error) {
+// var version int
+// if err := r.Read("version", &version); err != nil {
+// return nil, errors.Wrap(err, "Unable to read leave update")
+// }
+//
+// id, err := readUUID(r, "re")
+// if err != nil {
+// return nil, errors.Wrap(err, "Unable to read leave update")
+// }
+//
+// return &leave{id, version}, nil
+// }
+//
+// type join struct {
 // member Member
 // }
 //
-// func (p *fail) Re() uuid.UUID {
+// func (p *join) Re() uuid.UUID {
 // return p.member.Id()
 // }
 //
-// func (p *fail) Version() int {
+// func (p *join) Version() int {
 // return p.member.Version()
 // }
 //
-// func (p *fail) Apply(r Roster) bool {
-// return r.fail(p.member)
+// func (p *join) Write(w enc.Writer) {
+// w.Write("type", "join")
+// w.Write("member", p.member)
 // }
+//
+// func (p *join) Apply(r Roster) bool {
+// return r.join(p.member)
+// }
+//
+// func readJoin(ctx common.Context, r enc.Reader) (*join, error) {
+// var message enc.Message
+// if err := r.Read("member", &message); err != nil {
+// return nil, errors.Wrap(err, "Unable to read join update")
+// }
+//
+// member, err := readMember(ctx, message)
+// if err != nil {
+// return nil, errors.Wrap(err, "Unable to read join update")
+// }
+//
+// return &join{member}, nil
+// }
+//
+//
+// // func newFail(member Member) update {
+// // return &fail{member}
+// // }
+// //
+// // type fail struct {
+// // member Member
+// // }
+// //
+// // func (p *fail) Re() uuid.UUID {
+// // return p.member.Id()
+// // }
+// //
+// // func (p *fail) Version() int {
+// // return p.member.Version()
+// // }
+// //
+// // func (p *fail) Apply(r Roster) bool {
+// // return r.fail(p.member)
+// // }
